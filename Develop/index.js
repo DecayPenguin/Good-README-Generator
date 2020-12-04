1d09f36797946851419d89cbd5b9e2939bcfa4b7
Decay <devinamccomb@gmail.com>
2020-12-04 17:51:30 -0600

    Prompt changed, most logged but only title is put into the generated README

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,7 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
 // array of questions for user
 const questions = [
 
@@ -9,7 +13,68 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'projTitle',
+        message: 'What is your project title?',
+        default: 'Untitled Project',
+      },
+      {
+        type: 'input',
+        name: 'projType',
+        message: 'Describe your project.',
+        default: 'Something new',
+      },
+      {
+        type: 'input',
+        name: 'projTOC',
+        message: 'Supply a Table of Contents.',
+        default: 'Table of Contents',
+      },
+      {
+        type: 'input',
+        name: 'projInstall',
+        message: 'What is your project installation?',
+        default: '.json',
+      },
+      {
+        type: 'input',
+        name: 'projUse',
+        message: 'What is your projects usage purpose?',
+        default: 'Something new',
+      },
+      {
+        type: 'list',
+        name: 'projLicense',
+        message: 'Describe your project License.',
+        choices: ["CC", "GNU", "MIT"]
+      },
+      {
+        type: 'input',
+        name: 'projTests',
+        message: 'How do we test it?',
+        default: 'Use it',
+      },
+      {
+        type: 'input',
+        name: 'projQuestions',
+        message: 'Add',
+        default: 'questions',
+      }
+  
+  
+    ])
+    .then(answers => {
+      console.info('Answers:', answers);
+      var formatAnswers = `${answers.projTitle}`, ``
+      fs.writeFile('README.md', formatAnswers, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      });
+      
+    });
 }
 
 // function call to initialize program
